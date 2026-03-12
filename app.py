7aab6ca7983f4a27ed0510bd8ce189a71a2531b3
sheng372 <sheng.shangggg@gmail.com>
2026-03-12 11:31:00 +0800

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-return "Welcome to my Flask API!"
+    # Indentation is required here
+    return "Welcome to my Flask API!"
 
 @app.route('/student')
 def get_student():
-return jsonify({
-"name": "Your Name",
-"grade": 10,
-"section": "Zechariah"
-})
+    # Indentation is required here
+    return jsonify({
+        "name": "Your Name",
+        "grade": 10,
+        "section": "Zechariah"
+    })
+
+# This part tells Python to run the app when you execute the script
+if __name__ == '__main__':
+    app.run(debug=True)
